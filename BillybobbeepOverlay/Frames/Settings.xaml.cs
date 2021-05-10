b407341e9744fa055e86de19dfc6947890bfcec8
Tyler <tylerrp06@gmail.com>
2021-05-10 17:25:56 +0100

    Updated settings UI
    Bug fixes when saving options, catched any errors if not all data has been inputted & changed config file

--- BillybobbeepOverlay/Frames/Settings.xaml.cs
+++ BillybobbeepOverlay/Frames/Settings.xaml.cs
@@ -17,67 +17,95 @@ using System.Windows.Shapes;
 
 namespace BillybobbeepOverlay.Frames
 {
-    /// <summary>
-    /// Interaction logic for Settings.xaml
-    /// </summary>
-    public partial class Settings : Page
-    {
-        public Settings()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Interaction logic for Settings.xaml
+	/// </summary>
+	public partial class Settings : Page
+	{
+		public Settings()
+		{
+			InitializeComponent();
+			ReadConfig();
+		}
 
-        public async Task WriteCache(string text, string description, string imagePath)
-        {
-            if (File.Exists("Settings.config"))
-            {
-                TextWriter sw = new StreamWriter("Settings.config");
-                await sw.WriteLineAsync(string.Join(";", text,description, imagePath));
-                sw.Close();
-            }
-            else
-            {
-                File.CreateText("Settings.config");
-                TextWriter sw = new StreamWriter("Settings.config");
-                await sw.WriteLineAsync(string.Join(";", text, description, imagePath));
-                sw.Close();
-            }
-        }
-        private void UploadImage(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog fd = new OpenFileDialog();
-            fd.DefaultExt = ".png";
-            fd.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg";
-            if (fd.ShowDialog() == true)
-            {
-                string filename = fd.FileName;
-                ImageName.Text = filename;
-            }
-        }
+		public void ReadConfig()
+		{
+			if (File.Exists("Settings.billy"))
+			{
+				string line;
 
-        private void TopMostEnabled(object sender, RoutedEventArgs e)
-        {
-            Window mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.Topmost = true;
-        }
+				TextReader sr = new StreamReader("Settings.billy");
+				while ((line = sr.ReadLine()) != null)
+				{
+					var seperatedData = line.Split(new[] { ";" }, StringSplitOptions.None);
+					string title = seperatedData[0];
+					string description = seperatedData[1];
+					string image = seperatedData[2];
 
-        private void TopMostDisabled(object sender, RoutedEventArgs e)
-        {
-            Window mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.Topmost = false;
-        }
-        private void ReturnToHomePage(object sender, RoutedEventArgs e)
-        {
-            this.NavigationService.Navigate(new Overlay());
-        }
-        private async Task SaveSettings(object sender, RoutedEventArgs e)
-        {
-            await WriteCache(TitleInput.Text, DescInput.Text, ImageName.Text);
-            this.NavigationService.Navigate(new Overlay());
-        }
-        private void ShutdownApplication(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
-    }
+					try
+					{
+						TitleInput.Text = title;
+						DescInput.Text = description;
+						ImageName.Text = image;
+					}
+					catch
+					{
+						return;
+					}
+				}
+				sr.Close();
+			}
+		}
+		public async Task WriteCache(string text, string description, string imagePath)
+		{
+			if (File.Exists("Settings.billy"))
+			{
+				TextWriter sw = new StreamWriter("Settings.billy");
+				await sw.WriteLineAsync(string.Join(";", text, description, imagePath));
+				sw.Close();
+			}
+			else
+			{
+				File.CreateText("Settings.billy");
+				TextWriter sw = new StreamWriter("Settings.billy");
+				await sw.WriteLineAsync(string.Join(";", text, description, imagePath));
+				sw.Close();
+			}
+		}
+		private void UploadImage(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog fd = new OpenFileDialog();
+			fd.DefaultExt = ".png";
+			fd.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg";
+			if (fd.ShowDialog() == true)
+			{
+				string filename = fd.FileName;
+				ImageName.Text = filename;
+			}
+		}
+		private void TopMostEnabled(object sender, RoutedEventArgs e)
+		{
+			Window mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+			mainWindow.Topmost = true;
+		}
+
+		private void TopMostDisabled(object sender, RoutedEventArgs e)
+		{
+			Window mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+			mainWindow.Topmost = false;
+		}
+		private void ReturnToHomePage(object sender, RoutedEventArgs e)
+		{
+			this.NavigationService.Navigate(new Overlay());
+		}
+		private async void SaveSettings(object sender, RoutedEventArgs e)
+		{
+			await WriteCache(TitleInput.Text, DescInput.Text, ImageName.Text);
+			this.NavigationService.Navigate(new Overlay());
+		}
+		private void ShutdownApplication(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+	}
 }
